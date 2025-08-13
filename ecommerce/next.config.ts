6807f99dd64ce0d509cdabc2e73995d74e31a522
Bibek Ale Magar <magarbibek71@gmail.com>
2025-08-13 14:22:14 +0545

    Done

--- ecommerce/next.config.ts
+++ ecommerce/next.config.ts
@@ -2,9 +2,11 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
+  reactStrictMode: false,
+  trailingSlash: true,
   output: 'export',
-  basePath: isProd ? '/Quick-Pick' : '',
-  assetPrefix: isProd ? '/Quick-Pick/' : '',
+  basePath: isProd ? '/Quickpick' : '',
+  assetPrefix: isProd ? '/Quickpick/' : '',
   images: {
     unoptimized: true,
     domains: ['cdn.dummyjson.com', 'i.dummyjson.com'],
